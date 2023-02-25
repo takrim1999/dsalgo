e25026fd83a42b0a52105663728b9bfc80a9c09a
takrim1999 <takrimulalom@gmail.com>
2023-02-26 00:12:22 +0600

    Learning pointers

---
+++ C/prequisite/void_pointer.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int n = 10;
+    void *pointer = &n; // pointer is a void pointer here. It does not contain any type but can be initialized by any type 
+    printf("%d\n",*(int*)pointer); // this is called type casting, we are type casting to integer to print a value
+    // printf("%c\n",*(int*)pointer);
+    return 0;
+}
