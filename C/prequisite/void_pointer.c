65d9c5dfbd6aeb141d834ed9b9c76219b2b7bf99
takrim1999 <takrimulalom@gmail.com>
2023-03-07 01:39:17 +0600

    learning type casting

--- C/prequisite/void_pointer.c
+++ C/prequisite/void_pointer.c
@@ -2,8 +2,10 @@
 int main()
 {
     int n = 10;
+    char *na = "a";
     void *pointer = &n;              // pointer is a void pointer here. It does not contain any type but can be initialized by any type
     printf("%d\n", *(int *)pointer); // this is called type casting, we are type casting to integer to print a value
-    // printf("%c\n",*(int*)pointer);
+    pointer = na;
+    printf("%c\n", *(char *)pointer);
     return 0;
 }
