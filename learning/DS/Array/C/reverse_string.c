652611abd4564388a2e3f11931a1b54c29a29cd7
takrim1999 <takrimulalom@gmail.com>
2023-06-24 20:28:46 +0600

    Modified program with dynamic memory allocation

--- learning/DS/Array/C/reverse_string.c
+++ learning/DS/Array/C/reverse_string.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+int *reverse_array(int *point, int length)
+{
+    int *new_array = (int *)malloc(sizeof(int) * length);
+    printf("Reverse ");
+    int j = 0;
+    for (int i = length - 1; i >= 0; i--)
+    {
+        new_array[j] = *(point + i);
+        j++;
+    }
+    point = new_array;
+    return point;
+}
 
-void reverse_array(int *point, int length)
+void print(int *point, int length)
 {
     // printf("%d", *point);
-    length--;
-    for (int i = length; i >= 0; i--)
+    printf("Array is:\n");
+    for (int i = 0; i < length; i++)
     {
         printf("%d", *(point + i));
     }
+    printf("\n");
 }
 
 int main()
 {
-    int array[4] = {1, 2, 3, 4};
+    int array[6] = {1, 2, 3, 4, 5, 6};
     int *point = array;
     int length = sizeof(array) / sizeof(array[0]);
-    reverse_array(point, length);
+    print(point, length);
+    print(reverse_array(point, length), length);
     return 0;
 }
