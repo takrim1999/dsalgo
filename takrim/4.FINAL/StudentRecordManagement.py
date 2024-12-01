eb23c04c3bf5ec69cf854dcc6ebbabd51bd17456
takrim1999 <takrimulalom@gmail.com>
2024-12-01 21:09:35 +0600

    Updated interface and added more database functionalities

--- takrim/4.FINAL/StudentRecordManagement.py
+++ takrim/4.FINAL/StudentRecordManagement.py
@@ -3,9 +3,9 @@
 #         return "0" + str(num)
 #     else:
 #         return str(num)
-    
+import os
 
-class Manage:
+class StudentRecord:
     def __init__(self):
         self.studentList = []
         with open('objectDb.record', 'r') as database:
@@ -13,28 +13,80 @@ class Manage:
             for record in data:
                 self.studentList.append(eval(record))
     
-    def searchStudent(self,id=None):
-        parameter = input("Search parameter(roll,name,cgpa,position,age): ")
+    def searchStudent(self,parameter=None):
+        os.system("clear")
+        if not parameter:
+            parameter = input("Search parameter(roll,name,cgpa,position,age): ")
         if parameter == "name":
             name = input("Query Name: ")
+            payload = []
             for record in self.studentList:
                 for id, data in record.items():
                     if name.lower() in data['name'].lower():
-                        print(data)
+                        payload.append(data)
+            if not payload:
+                payload.append({'error': "No record found!"})
+            return payload
         if parameter == "cgpa":
-            cgpaquery = input("query: ")
-            if "<" in cgpaquery:
+            payload = []
+            cgpaquery = input("CGPA query([>,<,>=,<=,==]cgpa): ")
+            if ">=" in cgpaquery:
+                cg = float(cgpaquery.replace(">=",''))
+                for record in self.studentList:
+                    for id, data in record.items():
+                        if data['cgpa'] != 'NA' and float(data['cgpa'])>=cg:
+                            payload.append(data)
+            elif "<=" in cgpaquery:
+                cg = float(cgpaquery.replace("<=",''))
+                for record in self.studentList:
+                    for id, data in record.items():
+                        if data['cgpa'] != 'NA' and float(data['cgpa'])<=cg:
+                            payload.append(data)
+            elif "<" in cgpaquery:
                 cg = float(cgpaquery.replace("<",''))
                 for record in self.studentList:
                     for id, data in record.items():
                         if data['cgpa'] != 'NA' and float(data['cgpa'])<cg:
-                            print(data)
+                            payload.append(data)
+            elif ">" in cgpaquery:
+                cg = float(cgpaquery.replace(">",''))
+                for record in self.studentList:
+                    for id, data in record.items():
+                        if data['cgpa'] != 'NA' and float(data['cgpa'])>cg:
+                            payload.append(data)
+            elif "==" in cgpaquery:
+                cg = float(cgpaquery.replace("==",''))
+                for record in self.studentList:
+                    for id, data in record.items():
+                        if data['cgpa'] != 'NA' and float(data['cgpa'])==cg:
+                            payload.append(data)
+            if not payload:
+                payload.append({'error': "No record found!"})
+            
+            return payload
                 
+def display():
+    os.system("clear")
+    choice = input('''Welcome to ICE student Database!
+    type "exit" to exit
+    type "search" to search in database
+    type "add" to add record
+    type "edit" to edit a record!
+> ''')
+    return choice
 
-
-# s1 = Student("Md. Takrim Ul Alam",1911177149,25,3.3,20)
-# s2 = Student()
-# s1.getInfo()
-# s2.getInfo()
-management = Manage()
-management.searchStudent()
+while True:
+    choice = display()
+    myRecord = StudentRecord()
+    if choice == "exit":
+        break
+    elif choice == "search":
+        messages = myRecord.searchStudent()
+        for message in messages:
+            print(message)
+        while input("Search again?: ") == "yes":
+           messages = myRecord.searchStudent('cgpa')
+           for message in messages:
+               print(message)
+        else:
+            pass
